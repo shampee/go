66f61e39288439b078d5abfc4fcd40115900bbb1
Hampus O <shampee@x4.tel>
2021-03-24 05:33:59 +0100

    made stone_create and stone_destroy available

--- src/main.h
+++ src/main.h
@@ -1,9 +1,11 @@
 #include "common.h"
 
-extern void cleanup(void);
-extern void init_sdl(void);
-extern void do_input(void);
-extern Grid draw_grid(int grid_size, SDL_bool mouse_active,
-					  SDL_bool mouse_hover);
+extern void	  cleanup(void);
+extern void	  init_sdl(void);
+extern void	  do_input(void);
+extern Stone* stone_create(void);
+extern void	  stone_destroy(Stone* stone);
+extern Grid	  draw_grid(int grid_size, SDL_bool mouse_active,
+						SDL_bool mouse_hover);
 
 App app;
